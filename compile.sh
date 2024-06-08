4a9f4859a2910451c37b574aa50acb5259af8618
artisticmclaren <elijahjvandeventer@gmail.com>
2024-06-08 19:12:33 -0400

    attempting to cross compile to windows, not going well

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
 clear
-g++ main.cpp -o app -lsfml-graphics -lsfml-window -lsfml-system
-./app
+g++ -std=c++17 main.cpp -o distro/linux/GDedit-linux -lsfml-graphics -lsfml-window -lsfml-system # linux compilation
+echo "Linux successful."
+x86_64-w64-mingw32-g++ -std=c++17 main.cpp -o distro/win/GDedit-win -static-libgcc -static-libstdc++ -L/home/elijah/cpp/GDedit/lib -I/home/elijah/cpp/GDedit/include -lsfml-graphics -lsfml-window -lsfml-system # windows compilation
+echo "Windows successful."
+
+./distro/linux/GDedit-linux # run on linux
